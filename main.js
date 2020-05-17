77fd3aa6bae0e10e2b422c48e966f8b7e3ff1c73
Derrik Engel <derrik@derrikengel.com>
2020-05-16 19:25:19 -0500

    progress

--- main.js
+++ main.js
@@ -1,8 +1,75 @@
 $(function () {
-	$.getJSON("https://spreadsheets.google.com/feeds/list/1QrEHAN4o6dQe4PqCXg5_AkQ8u_j1nqt1GCpz90Lv5g4/od6/public/values?alt=json", function (data) {
-		var movies = data.feed.entry;
-		var template = $("#movie-template").html();
+	var googleSheet = 'https://spreadsheets.google.com/feeds/list/1QrEHAN4o6dQe4PqCXg5_AkQ8u_j1nqt1GCpz90Lv5g4/od6/public/values?alt=json',
+		template = $('#movie-template').html(),
+		$movieList = $('.js-movies'),
+		$sortOptions = $('.js-sort'),
+		$searchField = $('.js-search'),
+		$count = $('.js-count'),
+		$randomBtn = $('.js-random');
 
-		$("#movies").html(Mustache.to_html(template, data));
+	$.getJSON(googleSheet, function (data) {
+		// get json data from google sheets
+		movies = data.feed.entry;
+
+		// generate an index for each item
+		for (i in movies) {
+			movies[i].index = i;
+		}
+
+		// render the list with mustache
+		$movieList.html(Mustache.render(template, { movies: movies }));
+
+		sortMovies();
+
+		updateCount();
 	});
+
+	var sortMovies = function () {
+		var $cards = $movieList.find('.movie-card');
+
+		if ($sortOptions.val() == 'recent') {
+			// sort by 'acquired' data attribute
+			$cards.sort(function (a, b) {
+				return new Date($(b).data('acquired')) - new Date($(a).data('acquired'));
+			}).appendTo($movieList);
+		} else {
+			// sort by 'id' data attribute
+			$cards.sort(function (a, b) {
+				return ($(b).data('id')) < ($(a).data('id')) ? 1 : -1;
+			}).appendTo($movieList);
+		}
+	};
+
+	var randomMovie = function () {
+		var $cards = $movieList.find('.movie-card:visible, .movie-card[data-random-hidden]');
+		var random = Math.floor(Math.random() * $cards.length);
+
+		// add data attribute to keep trach of which items were hidden with randomize
+		// so randomize works multiples times with filtered results
+		$cards.attr('data-random-hidden', true).hide().eq(random).show();
+
+		updateCount();
+	};
+
+	var searchMovies = function () {
+		var searchTerm = $searchField.val();
+
+		$movieList.find('.movie-card').filter(function () {
+			return $(this).find('.movie-card-title').text().toLowerCase().indexOf(searchTerm) > -1;
+		}).show().end().filter(':visible').filter(function () {
+			return $(this).find('.movie-card-title').text().toLowerCase().indexOf(searchTerm) === -1;
+		}).hide();
+
+		updateCount();
+	}
+
+	var updateCount = function () {
+		$count.text($movieList.find('.movie-card:visible').length);
+	};
+
+	$sortOptions.on('change', sortMovies);
+
+	$searchField.on('input', searchMovies);
+
+	$randomBtn.on('click', randomMovie);
 });
