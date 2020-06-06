c37bc2b95ea77c7feb8cb8f7a801816bf41fa35a
Derrik Engel <derrik@derrikengel.com>
2020-06-06 09:29:40 -0500

    z-index issue, update screen pass data url

--- assets/main.js
+++ assets/main.js
@@ -119,7 +119,7 @@ var movies = new Vue({
             this.sort = 'alpha'
 
             // get screen pass data
-            axios.get('https://spreadsheets.google.com/feeds/list/1QrEHAN4o6dQe4PqCXg5_AkQ8u_j1nqt1GCpz90Lv5g4/4/public/values?alt=json')
+            axios.get('https://spreadsheets.google.com/feeds/list/1QrEHAN4o6dQe4PqCXg5_AkQ8u_j1nqt1GCpz90Lv5g4/3/public/values?alt=json')
                 .then(response => {
                     this.screenPassData = response.data.feed.entry
 
