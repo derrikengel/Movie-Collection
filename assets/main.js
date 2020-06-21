0eb0a6cb99c2be1cad34ec427b3fdb90be1ed0f6
Derrik Engel <derrik@derrikengel.com>
2020-06-21 12:55:22 -0500

    remove screen pass

--- assets/main.js
+++ assets/main.js
@@ -2,17 +2,7 @@ Vue.use(VueLazyload, {
     preLoad: 1.6
 })
 
-var router = new VueRouter({
-    routes: [
-        {
-            path: '/screen-pass',
-            name: 'screen-pass'
-        }
-    ]
-});
-
 var movies = new Vue({
-    router,
     el: '#movies',
     data() {
         return {
@@ -36,13 +26,6 @@ var movies = new Vue({
             minYear: 0,
             maxYear: 0,
             randomMovie: false,
-            screenPass: false,
-            screenPassData: null,
-            screenPassUser: null,
-            screenPassAvailable: true,
-            screenPassStatus: null,
-            screenPassCounter: null,
-            currentMonth: null,
             requestFormActive: false,
             requestName: null,
             requestTitle: null,
@@ -81,12 +64,6 @@ var movies = new Vue({
                 this.movieData = response.data.feed.entry
                 this.movieData = Object.freeze(this.movieData);
 
-                if (this.$route.name == 'screen-pass')
-                    this.movieData = this.movieData.filter(movie => movie.gsx$screenpass.$t == 'Yes')
-
-                if (readCookie('movieRequester'))
-                    this.requestName = readCookie('movieRequester')
-
                 // set up filters
                 this.allGenres = [...new Set(this.movieData.flatMap(movie => movie.gsx$genre.$t.split(', ')))].sort()
                 this.minYear = _.min(this.movieData.flatMap(movie => movie.gsx$year.$t))
@@ -115,51 +92,11 @@ var movies = new Vue({
                 this.errored = true
             })
             .finally(() => this.loading = false)
-        
-        if (this.$route.name == 'screen-pass') {
-            this.screenPass = true
-            this.sort = 'alpha'
-
-            // get screen pass data
-            axios.get('https://spreadsheets.google.com/feeds/list/1QrEHAN4o6dQe4PqCXg5_AkQ8u_j1nqt1GCpz90Lv5g4/3/public/values?alt=json')
-                .then(response => {
-                    this.screenPassData = response.data.feed.entry
-
-                    if (readCookie('screenPassRequested'))
-                        this.screenPassAvailable = false
-
-                    // get current date, first day of current month and last day of current month
-                    var currentDate = new Date()
-                    currentDate.setHours(0, 0, 0, 0)
-                    var currentMonth = currentDate.getMonth()
-                    var firstDay = new Date(currentDate.getFullYear(), currentMonth, 1)
-                    var lastDay = new Date(currentDate.getFullYear(), currentMonth + 1, 0)
-
-                    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-                    this.currentMonth = months[currentMonth]
-
-                    // get the requests for the current month
-                    var currentRequests = this.screenPassData.filter(request => {
-                        var requestDate = new Date(request.gsx$timestamp.$t).setHours(0, 0, 0, 0)
-                        return requestDate >= firstDay && requestDate <= lastDay
-                    })
-                    
-                    // set the number of remaining passes
-                    this.screenPassCounter = currentRequests.length <= 3 ? currentRequests.length - 3 : 0;
-
-                    if (this.screenPassCounter < 1)
-                        this.screenPassAvailable = false
-                })
-        }
     },
     methods: {
         selectItem(index) {
             // handle click for movie cards
             index === this.activeCard ? this.activeCard = null : this.activeCard = index
-
-            // focus not working?
-            // if (this.screenPass && index === this.activeCard)
-            //     this.$refs.email[index].focus()
 
             this.activeFilter = null
 
@@ -202,32 +139,6 @@ var movies = new Vue({
 
             // hide filter menus
             this.activeFilter = null
-        },
-        submitScreenPass(movie, year) {
-            // MA implemented their own list sharing, so removing this for now
-            // const scriptURL = 'https://script.google.com/macros/s/AKfycbzDLlzElZKNrEPIcrnQNV9P6duLdkufuq_g8QQSSTgi0yHvU3Y/exec'
-            // const formData = new FormData()
-            // formData.append('Requester', this.screenPassUser)
-            // formData.append('Movie', movie + ' (' + year + ')' )
-
-            // this.screenPassStatus = 'processing'
-            
-            // fetch(scriptURL, { method: 'POST', body: formData })
-            //     .then(result => {
-            //         this.screenPassStatus = 'success'
-            //         this.screenPassAvailable = false
-
-            //         // set to expire cookie first day of next month
-            //         var date = new Date();
-            //         date.setMonth(date.getMonth() + 1, 1)
-
-            //         // set a cookie to expire the first day of next month so a user can only request 1 per month
-            //         setCookie('screenPassRequested', 'true', date)
-            //     })
-            //     .catch(error => {
-            //         console.error(error.message)
-            //         this.screenPassStatus = 'error'
-            //     })
         },
         toggleRequestForm(e) {
             this.requestFormActive = !this.requestFormActive
