b37f28be01015c82b6b35e0f72174efb832bfc4e
Derrik Engel <dengel@wearediagram.com>
2026-03-13 09:00:52 -0500

    PWA cleanup

--- vite.config.js
+++ vite.config.js
@@ -25,13 +25,13 @@ export default defineConfig({
                         src: '/icon-192.png',
                         sizes: '192x192',
                         type: 'image/png',
-                        purpose: 'maskable'
+                        purpose: 'any maskable'
                     },
                     {
                         src: '/icon-512.png',
                         sizes: '512x512',
                         type: 'image/png',
-                        purpose: 'maskable'
+                        purpose: 'any maskable'
                     }
                 ]
             },
